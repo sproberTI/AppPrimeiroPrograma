3f36c774fd2700805589eaec9be9a59ebb881323
Roberto <rober.autom@gmail.com>
2022-09-11 08:18:23 -0300

    Colocando entrada de dados (readLine()!!.)

--- AppPrimeiroProjeto/src/main/kotlin/PrimeiroPrograma.kt
+++ AppPrimeiroProjeto/src/main/kotlin/PrimeiroPrograma.kt
@@ -1,5 +1,21 @@
-fun main(){
+fun main() {
     //Meu Primeiro Programa
-    print("1º Programa Kotlin, Roberto Santos.")
+    println("1º Programa Kotlin, Roberto Santos.")
+
+    println("Qual é o seu nome? ")
+
+    var nome: String = readLine()!!.toString()
+
+    println("Olá, $nome. Muito prazer...")
+
+    println("Qual é a sua idade? ")
+
+    var idade: Int = readLine()!!.toInt()
+
+    println("Sua idade é $idade")
+
+    println()
+
+    println("Seu nome é $nome e você tem $idade anos.")
 
 }
